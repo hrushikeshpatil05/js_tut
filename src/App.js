b657860bf95fc7ebea6a5d90bfb9b53c402ee261
Hrushikesh <hrushipatil2019262@gmail.com>
2023-06-24 14:55:29 -0700

    learned how does the timeout works using web api's

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hii
-    </div>
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-
+function createEventListener(){
+  let count = 0;
+  function a(){
+    console.log("hii");
+  }
+  var t = function() {document.getElementById("clickMe").addEventListener("click", function xyz(){ //when event click occurs, this callback function (xyz) is called into callstack
+  console.log("Button clicked",++count);
+  function b(){
+    console.log("hello");
+  }
+  let res2 = window.setTimeout(a,3000);
+  let res = window.setTimeout(b,3000);
+  
+});}
+// t();
+let res3 = window.setTimeout(t,5000);
+}
+createEventListener();
